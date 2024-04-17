1170a051a890e5428a3afbe3dd4dcbf21fca5531
shai zaedamn <shai@spectrogem.com>
2024-04-17 14:52:23 +0300

    star work on caricature

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-mod caricature;
+use caricatures::caricature::Caricature;
 
+mod caricatures;
 
 fn main() {
-    let mut  hero = caricature::Caricature { life: 10, attack: 5, defense: 2 };
-    let mut monster=caricature::Caricature{ life: 10, attack: 5, defense: 2 };
-    for _ in 1..10{    monster.get_damage(hero.attack)}
-
-
+    let mut hero = Caricature { life: 10, attack: 5, defense: 2 };
+    let mut monster = Caricature { life: 15, attack: 5, defense: 2 };
+    for _ in 1..10 { monster.get_damage(hero.attack) }
 }
